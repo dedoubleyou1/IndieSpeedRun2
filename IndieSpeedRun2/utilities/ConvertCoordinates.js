a7021c36a5beef715a904bb0025aa54fe1a6dab9
David Wallin <dedoubleyou1@gmail.com>
2013-09-21 23:17:11 -0400

    Revert "conflict"
    This reverts commit 06c049e2b50041f2f1acbd36c89f416de3c98606.

--- IndieSpeedRun2/utilities/ConvertCoordinates.js
+++ IndieSpeedRun2/utilities/ConvertCoordinates.js
@@ -9,11 +9,7 @@ utilities.ConvertCoordinates = function(wedge,row,column)
     var startAngle = wedge * (Math.PI/4);
     
     //use ROW to figure out the "radius" of that row
-<<<<<<< HEAD
     var triangleHeight = 10;
-=======
-    var triangleHeight = 6;
->>>>>>> e9f3402ce5a0a3de4bace005c7bb36423e36e733
     var rowRadius = triangleHeight/2 + row*(triangleHeight/2);
     
     //get the two SIDE vectors of this particular wedge @ rowRadius
@@ -23,27 +19,15 @@ utilities.ConvertCoordinates = function(wedge,row,column)
     //get the center line of this 
     var centerEdge = goog.math.Vec2.difference(sideVector2,sideVector1);
     
-    //use ROW to figure out how to break up this vector into mini segments
-    var numberOfSegments = (row*2+1)*2;
-    var ratioOfSegment = 1/numberOfSegments;
+    //use ROW to figure out how to break up this vector (# of columns + 1)
+    var numberOfSegments = (row+1)*2;
     
     //use COLUMN to figure out how to scale the vector
-//    centerEdge.scale((column+1) * 1/numberOfSegments);
-    centerEdge.scale(ratioOfSegment + column*2*ratioOfSegment);
-
-    
-    var triangleCenter = goog.math.Vec2.sum(sideVector1,centerEdge);
-    
-    var offset = triangleHeight/16;
+    centerEdge.scale((column+1) * 1/numberOfSegments);
     
     //find OFFSET vector based on odd or even?
-    if(column%2 == 0){
-        triangleCenter.add(triangleCenter.clone().normalize().scale(offset));
-    }else{
-        triangleCenter.add(triangleCenter.clone().normalize().scale(-offset));
-    }
     
-    return triangleCenter;
+    return goog.math.Vec2.sum(sideVector1,centerEdge);
 };
 
 goog.exportSymbol('utilities.ConvertCoordinates',utilities.ConvertCoordinates);
