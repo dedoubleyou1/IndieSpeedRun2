836ee6601843bed15be34ea57adbeb25110c7f3d
David Wallin <dedoubleyou1@gmail.com>
2013-09-22 10:22:39 -0400

    Updated for loop with closure

--- IndieSpeedRun2/utilities/Level.js
+++ IndieSpeedRun2/utilities/Level.js
@@ -19,27 +19,30 @@ utilities.Level = function(size, triangleHeight) {
   var toppings = new lime.Layer().setPosition(512, 384);
   var levelData = new utilities.NewStruct(size);
   
-  
+  function initToppingsFunc(i, j, k) {   
+    return function() {
+
+      levelData.add(i, j, k, {junk: "data"});
+      var myCoordinates = utilities.ConvertCoordinates(i, j, k, triangleHeight);
+      var newCircle = new lime.Circle().setSize(38,38).setFill(0,0,0,0).setPosition(20 * myCoordinates.x, 20 * myCoordinates.y);
+      toppings.appendChild(newCircle);
+
+      goog.events.listen(newCircle,'click', function(e){
+          newCircle.setFill(30*i,90*j,60*k)
+          console.log(i, j, k);
+      })
+    };
+  }
+
   for (var i = 7; i >= 0; i--) {
     for (var j = size - 1; j >= 0; j--) {
       for (var k = j * 2; k >= 0; k--) {
-        levelData.add(i, j, k, {junk: "data"});
-        var myCoordinates = utilities.ConvertCoordinates(i, j, k, triangleHeight);
-        var newCircle = new lime.Circle().setSize(38,38).setFill(i * 30, j * 90, k * 60).setPosition(20 * myCoordinates.x, 20 * myCoordinates.y);
-                
 
-        goog.events.listen(newCircle,'click', function(e){
-            console.log(tempholder().i, tempholder().j, tempholder().k);
-        })
-        
-        
-        toppings.appendChild(newCircle);
-        console.log(i, j, k, myCoordinates);
+        (initToppingsFunc(i, j, k))();
+
       }
     }
   }
-  
-  
 
   newLevel.appendChild(toppings);
   this.levelScene = newLevel;
