b57cadeb88a9d96f42ac910323c971147f64dfef
rsc390 <rohit@Rohits-MacBook-Pro.local>
2013-09-22 14:15:21 -0400

    chainingMushrooms function

--- IndieSpeedRun2/utilities/Topping.js
+++ IndieSpeedRun2/utilities/Topping.js
@@ -2,21 +2,37 @@ goog.provide('utilities.Topping');
 
 var toppingTypes = {
   pepperoni: {
-    image: 'assets/toppings_pepperoni.png
+    image: 'assets/toppings_pepperoni.png',
+    chaining: function(levelData, i, j, k){
+      
+    }
     
   },
+  
   mushroom: {
     image: 'assets/toppings_mushroom.png',
+    chaining: function(levelData, i, j, k){
+        this.toppingType = 'pepperoni';
+        this.sprite.setFill(utilities.Topping('pepperoni').image);
+      
+        attackNeighbors(levelData, i, j, k);
+    }
+  },
+  
+  olive: {
+    image: 'assets/toppings_olive.png',
     chaining: function(levelData, i, j, k){
       
     }
   },
-  olive: {
-    image: 'assets/toppings_olive.png'
-  },
+  
   anchovy: {
-    image: 'assets/toppings_anchovy.png'
+    image: 'assets/toppings_anchovy.png',
+    chaining: function(levelData, i, j, k){
+      
+    }
   }
+  
   // bacon: 4,
   // buffaloChicken: 5,
 
@@ -27,7 +43,23 @@ utilities.Topping = function(type)
     return toppingTypes[type];
 };
 
+//dat local neighbor check
+function attackNeighbors(levelData, i,j,k)
+{
+  var neighborList = levelData.neighbors(i,j,k);
+                
+  for(i=0; i<neighborList.length; i++){
+    //console.log(neighborList);
+    var target= levelData.get(neighborList[i].wedge, neighborList[i].row, neighborList[i].column);
+    
+    if(target.isOccupied)
+      target.chaining(levelData,i,j,k);
+  }
+}
+
+
 goog.exportSymbol('utilities.Topping',utilities.Topping);
+
 
 /*
 var attackNeighbors = function(i,j,k)
