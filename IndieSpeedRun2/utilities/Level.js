a075d5a2914d6167576ed66b7090fb7f3f3b587a
rsc390 <rohit@Rohits-MacBook-Pro.local>
2013-09-22 01:08:52 -0400

    click testing

--- IndieSpeedRun2/utilities/Level.js
+++ IndieSpeedRun2/utilities/Level.js
@@ -18,17 +18,28 @@ utilities.Level = function(size, triangleHeight) {
   newLevel.appendChild(levelBackground);
   var toppings = new lime.Layer().setPosition(512, 384);
   var levelData = new utilities.NewStruct(size);
+  
+  
   for (var i = 7; i >= 0; i--) {
     for (var j = size - 1; j >= 0; j--) {
       for (var k = j * 2; k >= 0; k--) {
         levelData.add(i, j, k, {junk: "data"});
         var myCoordinates = utilities.ConvertCoordinates(i, j, k, triangleHeight);
         var newCircle = new lime.Circle().setSize(38,38).setFill(i * 30, j * 90, k * 60).setPosition(20 * myCoordinates.x, 20 * myCoordinates.y);
+                
+
+        goog.events.listen(newCircle,'click', function(e){
+            console.log(tempholder().i, tempholder().j, tempholder().k);
+        })
+        
+        
         toppings.appendChild(newCircle);
         console.log(i, j, k, myCoordinates);
       }
     }
   }
+  
+  
 
   newLevel.appendChild(toppings);
   this.levelScene = newLevel;
