23fd8f82ec44b42a565e824d29518ed79c3c497b
David Wallin <dedoubleyou1@gmail.com>
2013-09-22 18:29:06 -0400

    Linted

--- IndieSpeedRun2/utilities/Level.js
+++ IndieSpeedRun2/utilities/Level.js
@@ -2,12 +2,12 @@ goog.provide('utilities.Level');
 
 
 //get requirements
-goog.require('lime.Scene');
-goog.require('lime.Layer');
 goog.require('lime.Circle');
+goog.require('lime.Layer');
+goog.require('lime.Scene');
 goog.require('lime.Sprite');
-goog.require('utilities.NewStruct');
 goog.require('utilities.ConvertCoordinates');
+goog.require('utilities.NewStruct');
 goog.require('utilities.Topping');
 
 utilities.Level = function(size, triangleHeight) {
@@ -20,22 +20,22 @@ utilities.Level = function(size, triangleHeight) {
   var toppings = new lime.Layer().setPosition(512, 384);
   var levelData = new utilities.NewStruct(size);
   var powerUps = {};
-  
-  function initToppingsFunc(wedge, row, column) {   
+
+  function initToppingsFunc(wedge, row, column) {
     return function() {
 
       var myCoordinates = utilities.ConvertCoordinates(wedge, row, column, triangleHeight);
-      var newCircle = new lime.Circle().setSize(40,40).setFill(0,0,0,0).setPosition(myCoordinates.x, myCoordinates.y);
+      var newCircle = new lime.Circle().setSize(40, 40).setFill(0, 0, 0, 0).setPosition(myCoordinates.x, myCoordinates.y);
       toppings.appendChild(newCircle);
 
       levelData.add(wedge, row, column, {toppingType: 'empty', sprite: newCircle, isOccupied: false});
 
       //HANDLE mouse clicks
-      goog.events.listen(newCircle,'click', function(e){
-          
+      goog.events.listen(newCircle, 'click', function(e) {
+
           var thisTopping = levelData.get(wedge, row, column);
 
-          if(!thisTopping.isOccupied){
+          if (!thisTopping.isOccupied) {
             //place a PEPPERONI
             newCircle.setFill(utilities.Topping('pepperoni').image); //.setFill(30*i,90*row,60*k);
             var thisTopping = levelData.get(wedge, row, column);
@@ -46,15 +46,15 @@ utilities.Level = function(size, triangleHeight) {
             var powerUpProperties = Object.getOwnPropertyNames(powerUps);
             for (var i = powerUpProperties.length - 1; i >= 0; i--) {
             //  powerUps[powerUpProperties[i]]
-            };
-            
+            }
+
             //Check Neighbor Triggers
             resultsArray = [];
             var resultsObject = {};
             for (var x = neighborList.length - 1; x >= 0; x--) {
               var neighborData = levelData.get(neighborList[x].wedge, neighborList[x].row, neighborList[x].column);
               if (neighborData.isOccupied) {
-                var temp = utilities.Topping(neighborData.toppingType).chaining(levelData,neighborList[x].wedge, neighborList[x].row, neighborList[x].column, resultsObject); //wedge, row, column);
+                var temp = utilities.Topping(neighborData.toppingType).chaining(levelData, neighborList[x].wedge, neighborList[x].row, neighborList[x].column, resultsObject); //wedge, row, column);
                 resultsArray.push(temp);
               }
             }
@@ -62,7 +62,7 @@ utilities.Level = function(size, triangleHeight) {
             //Tally up results from placement
             finalTally = {};
             for (var i = resultsArray.length - 1; i >= 0; i--) {
-              var propertyNames = Object.getOwnPropertyNames(resultsArray[i])
+              var propertyNames = Object.getOwnPropertyNames(resultsArray[i]);
               for (var j = propertyNames.length - 1; j >= 0; j--) {
                 if (finalTally.hasOwnProperty(propertyNames[j])) {
                   finalTally[propertyNames[j]] += resultsArray[i][propertyNames[j]];
@@ -71,9 +71,9 @@ utilities.Level = function(size, triangleHeight) {
                 }
               }
             }
-            powerUps= = finalTally;
+            powerUps = finalTally;
           }
-      })
+      });
     };
   }
 
@@ -86,12 +86,12 @@ utilities.Level = function(size, triangleHeight) {
       }
     }
   }
-  
+
   randomizeLevel(size, triangleHeight, levelData, toppings);
 
   newLevel.appendChild(toppings);
   this.levelScene = newLevel;
-}
+};
 
 function randomizeLevel(size, triangleHeight, levelData, toppings) {
   var rand = 0;
@@ -100,30 +100,29 @@ function randomizeLevel(size, triangleHeight, levelData, toppings) {
     for (var row = size - 1; row >= 0; row--) {
       for (var column = row * 2; column >= 0; column--) {
         rand = Math.random();
-        if(rand < .2){
+        if (rand < .2) {
           var myCoordinates = utilities.ConvertCoordinates(wedge, row, column, triangleHeight);
           levelData.get(wedge, row, column).sprite.setFill(utilities.Topping('mushroom').image);
           levelData.get(wedge, row, column).isOccupied = true;
           levelData.get(wedge, row, column).toppingType = 'mushroom';
         }
-        else if(rand >= .2 && rand < .3){
+        else if (rand >= 0.2 && rand < 0.3) {
           var myCoordinates = utilities.ConvertCoordinates(wedge, row, column, triangleHeight);
-          
+
           levelData.get(wedge, row, column).sprite.setFill(utilities.Topping('olive').image);
           levelData.get(wedge, row, column).isOccupied = true;
           levelData.get(wedge, row, column).toppingType = 'olive';
         }
-        else if(rand >= .5 && rand < .55){
+        else if (rand >= 0.5 && rand < 0.55) {
           var myCoordinates = utilities.ConvertCoordinates(wedge, row, column, triangleHeight);
-          
+
           levelData.get(wedge, row, column).sprite.setFill(utilities.Topping('anchovy').image);
           levelData.get(wedge, row, column).isOccupied = true;
-          levelData.get(wedge, row, column).toppingType = 'anchovy';        
+          levelData.get(wedge, row, column).toppingType = 'anchovy';
         }
       }
     }
   }
-  
 }
 
 goog.exportSymbol('utilities.Level', utilities.Level);
