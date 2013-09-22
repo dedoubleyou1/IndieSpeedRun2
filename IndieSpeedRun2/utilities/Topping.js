c6a4896df1ef693d4bc95be9bfe4517c41feada9
David Wallin <dedoubleyou1@gmail.com>
2013-09-22 13:59:40 -0400

    toppings cleaning

--- IndieSpeedRun2/utilities/Topping.js
+++ IndieSpeedRun2/utilities/Topping.js
@@ -21,40 +21,6 @@ var toppingTypes = {
 utilities.Topping = function(type)
 {
     return toppingTypes[type];
-// //    var image = new lime.Sprite();
-//     var image = new lime.Circle().setSize(40,40).setFill('#FFFFFF').
-    
-//     //resistance = how much enemy dominance this topping can withstand
-//     var resistance = 1;
-//     var dominance = 0; 
-    
-//     //whether or not this topping is being dominated by a pepperoni
-//     TYPE = type;
-    
-//     /* 
-//         influential: 1,
-//         dominant: 2,
-//         infludominant: 3,
-//         bomb: 4
-//     */ 
 };
-
-//returns type
-utilities.Topping.prototype.getType = function()
-{
-    return this.TYPE;
-}
-
-//
-utilities.Topping.prototype.addDominance = function(domToAdd)
-{
-    dominance += domToAdd;
-    
-    if(dominance >= resistance)
-    {
-        turn 
-        TYPE = 1;
-    }
-}
 
 goog.exportSymbol('utilities.Topping',utilities.Topping);
