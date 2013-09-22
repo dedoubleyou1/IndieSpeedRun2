b04c5bda3aabb3101e6d0ecf62c8274845254698
rsc390 <rohit@Rohits-MacBook-Pro.local>
2013-09-22 14:37:00 -0400

    Topping - (i,j,k) > (wedge, row, column)

--- IndieSpeedRun2/utilities/Topping.js
+++ IndieSpeedRun2/utilities/Topping.js
@@ -3,7 +3,7 @@ goog.provide('utilities.Topping');
 var toppingTypes = {
   pepperoni: {
     image: 'assets/toppings_pepperoni.png',
-    chaining: function(levelData, i, j, k){
+    chaining: function(levelData, wedge, row, column){
       
     }
     
@@ -11,24 +11,24 @@ var toppingTypes = {
   
   mushroom: {
     image: 'assets/toppings_mushroom.png',
-    chaining: function(levelData, i, j, k){
+    chaining: function(levelData, wedge, row, column){
         this.toppingType = 'pepperoni';
         this.sprite.setFill(utilities.Topping('pepperoni').image);
       
-        attackNeighbors(levelData, i, j, k);
+        attackNeighbors(levelData, wedge, row, column);
     }
   },
   
   olive: {
     image: 'assets/toppings_olive.png',
-    chaining: function(levelData, i, j, k){
+    chaining: function(levelData, wedge, row, column){
       
     }
   },
   
   anchovy: {
     image: 'assets/toppings_anchovy.png',
-    chaining: function(levelData, i, j, k){
+    chaining: function(levelData, wedge, row, column){
       
     }
   }
@@ -44,16 +44,16 @@ utilities.Topping = function(type)
 };
 
 //dat local neighbor check
-function attackNeighbors(levelData, i,j,k)
+function attackNeighbors(levelData, wedge,row,column)
 {
-  var neighborList = levelData.neighbors(i,j,k);
+  var neighborList = levelData.neighbors(wedge,row,column);
                 
   for(i=0; i<neighborList.length; i++){
     //console.log(neighborList);
-    var target= levelData.get(neighborList[i].wedge, neighborList[i].row, neighborList[i].column);
+    var target= levelData.get(neighborList[i].wedge,, neighborList[i].row, neighborList[i].column);
     
     if(target.isOccupied)
-      target.chaining(levelData,i,j,k);
+      target.chaining(levelData,wedge,row,column);
   }
 }
 
@@ -62,13 +62,13 @@ goog.exportSymbol('utilities.Topping',utilities.Topping);
 
 
 /*
-var attackNeighbors = function(i,j,k)
+var attackNeighbors = function(wedge,row,column)
             {
-                var neighborList = levelData.neighbors(i,j,k);
+                var neighborList = levelData.neighbors(wedge,row,column);
                 
                 for(i=0; i<neighborList.length; i++){
                   console.log(neighborList);
-                  var target= levelData.get(neighborList[i].wedge, neighborList[i].row, neighborList[i].column);
+                  var target= levelData.get(neighborList[i].wedge,, neighborList[i].row, neighborList[i].column);
                   
                   if(!target.isOccupied)
                     return;
@@ -76,11 +76,11 @@ var attackNeighbors = function(i,j,k)
                   if(target.toppingType == 'mushroom'){
                     target.toppingType = 'pepperoni';
                     target.sprite.setFill(utilities.Topping('pepperoni').image);
-                    attackNeighbors(neighborList[i].wedge, neighborList[i].row, neighborList[i].column);
+                    attackNeighbors(neighborList[i].wedge,, neighborList[i].row, neighborList[i].column);
                   }
                 }
             }
             
-attackNeighbors(i,j,k);
+attackNeighbors(wedge,row,column);
 
 */
