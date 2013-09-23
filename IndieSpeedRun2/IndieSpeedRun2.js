f194064e87b071b2ba293a686e31eb26468d724c
rsc390 <rohit@Rohits-MacBook-Pro.local>
2013-09-23 00:24:47 -0400

    nothing

--- IndieSpeedRun2/IndieSpeedRun2.js
+++ IndieSpeedRun2/IndieSpeedRun2.js
@@ -22,7 +22,7 @@ IndieSpeedRun2.start = function() {
   var director = new lime.Director(document.body,1024,768);
 
   var toppingChances = {
-    mushroom: .25,
+    mushroom: .3,
     olive: .15,
     anchovy: .05
   }
